80ed38694425d361e8df538cc70ebbde8225783e
ada1012 <75269153+ada1012@users.noreply.github.com>
2023-03-13 20:31:36 +0100

    Step 1

--- VehiculosAlbertoDiaz.sql
+++ VehiculosAlbertoDiaz.sql
@@ -1,5 +1,11 @@
 create or replace procedure alquilar(arg_NIF_cliente varchar,
   arg_matricula varchar, arg_fecha_ini date, arg_fecha_fin date) is
 begin
-  null;
+    if arg_fecha_fin < arg_fecha_ini then
+        raise_application_error(-20003, 'El numero de dias sera mayor que 0');
+    end if;
 end;
+/
+
+set serveroutput on
+exec test_alquila_coches;
